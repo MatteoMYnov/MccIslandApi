c3136a8319fc4dca5b59cae6774cc8988db8919d
mmatteo <matteo.martin@ynov.com>
2025-01-24 15:30:57 +0100

    scroll in modal, freeze background when in modal, new capes descriptions

--- site/js/modal.js
+++ site/js/modal.js
@@ -59,10 +59,25 @@ const modalData = {
         "date": "Obtentions: 17 mai 2024 au 18 juin 2024",
         "description": "Cette cape a été offerte aux joueurs qui ont échangé le code lors de l'événement du 15e anniversaire de Minecraft. Les utilisateurs peuvent débloquer un code échangeable pour la cape en regardant un livestream Minecraft sur TikTok. En fonction de la région de l'utilisateur, des conditions supplémentaires étaient imposées, comme regarder pendant un certain nombre de minutes ou taper « minecraft » dans le chat en direct. L'événement a commencé le 17 mai 2024 et s'est terminé le 18 juin 2024. Les joueurs qui ont débloqué la cape l'ont d'abord reçue sur l'édition Bedrock, puis l'ont progressivement étendue à l'édition Java en juillet 2024. Tous les codes pour la cape ont été désactivés le 30 juin 2024. Les joueurs vivant dans des régions où TikTok est interdit ne pouvaient pas déverrouiller la cape, à moins qu'un autre joueur ne leur donne un code. Le 12 juin 2024, un joueur sur Java Edition, sandycheeksvore, a été repéré avec la cape, qui a ensuite été révoquée le 15 juin 2024. Le 1er juillet 2024, la cape a été lentement distribuée à tous les joueurs éligibles",
     },
-    "twitch":{
+    "twitch": {
         "name": "Purple Heart Cape",
         "date": "Obtentions: 15 mai 2024 au 31 mai 2024",
         "description": "Cette cape a été offerte aux joueurs qui ont échangé le code lors de l'événement du 15e anniversaire de Minecraft. Les utilisateurs pouvaient débloquer un code Twitch Drop pour la cape en regardant n'importe quel streamer Minecraft avec les Drops activés pendant au moins 15 minutes. Le Drop a été lancé le 15 mai 2024 et s'est terminé le 31 mai 2024. Tous les codes pour la cape ont été désactivés le 30 juin 2024. Les joueurs qui ont débloqué la cape l'ont d'abord reçue sur l'édition Bedrock, avant de l'étendre progressivement à l'édition Java en juillet 2024. Le 20 mai 2024, un joueur sur Java Edition, _Kaptanbey0_, a été repéré avec la cape, qui a ensuite été révoquée le 15 juin 2024. Le 1er juillet 2024, la cape a été lentement distribuée à tous les joueurs éligibles."
+    },
+    "cherry": {
+        "name": "Cherry Cape",
+        "date": "Obtentions: 12 octobre 2023",
+        "description": "Cette cape a été offerte à tous ceux qui ont rejoint le serveur de l'événement Mob Vote 2023 ou qui ont voté depuis le launcher Minecraft."
+    },
+    "mcc": {
+        "name": "MCC 15th Year Cape",
+        "date": "Obtentions: 5 août 2024",
+        "description": "Cette cape a été remise aux joueurs qui ont relevé les 15 défis du serveur de l'événement des mini-jeux de MCChampionship, l'événement a débuté le 22 juillet 2024. 10 des défis demandent au joueur d'atteindre certaines statistiques dans les mini-jeux, et 4 sont pour compléter chacun des mini-jeux. Le défi restant est la Grotte Mystère, qui consiste en 7 tâches différentes suggérées par les dessins de la grotte. À partir du 22 juillet 2024, une nouvelle tâche a été révélée chaque jour, ce qui a permis d'obtenir la cape du 28 juillet 2024 au 5 août 2024. Bien qu'il ait été initialement prévu qu'il soit exclusif à l'édition Bedrock, il a été déclaré qu'il serait également disponible pour l'édition Java avec un court délai, suite aux commentaires de la communauté. Il a été rendu disponible pour Java après la conclusion de l'événement à partir du 5 août 2024, comme cela avait été annoncé précédemment."
+    },
+    "mojangoffice": {
+        "name": "Mojang Office Cape",
+        "date": "7 décembre 2024",
+        "description": "Cette cape a fait l'objet d'une fuite le 25 novembre 2024 sur le site web de Minecraft avec le titre « Achetez une cape, obtenez une cape » La cape a été retirée de la page quelques heures plus tard. Mais les joueurs ont pu comprendre qu'en achetant une cape de Creeper de n'importe quelle taille dans la boutique officielle de Minecraft à partir du 7 décembre 2024, vous étiez censé recevoir un code pour déverrouiller une cape exclusive dans le jeu appelée la cape Eyeblossom. Le 28 novembre 2024, la page de la boutique pour la cape Creeper a été mise à jour pour inclure l'information que l'achat de cette cape débloquerait une cape exclusive dans le jeu. Cependant, cet extrait a ensuite été supprimé de la page et remplacé par une promotion incluant un objet Bedrock Character Creator peu de temps après. Le 9 décembre 2024, Mojang a annoncé le serveur événementiel Eerie Mojang Office Party qui, comme indiqué dans l'article, donnerait aux joueurs la cape après avoir terminé 5 défis quotidiens Le nom de la cape a également été révélé comme étant la « Mojang Office cape ». Le 7 décembre 2024, un employé de Mojang a confirmé que la cape serait multiplateforme et à 9 h 23 EST le même jour, elle a été ajoutée au back-end de la Java Edition. Après avoir terminé le cinquième et dernier défi quotidien sur le serveur d'événements, les joueurs pouvaient immédiatement accéder à la cape dans les deux versions du jeu."
     }
 };
 
@@ -73,11 +88,11 @@ function openModal(event) {
     const modalName = document.querySelector(".modal-name");
     const modalDate = document.querySelector(".modal-date");
     const modalDesc = document.querySelector(".modal-desc");
-    const clickedImage = event.target;  // L'image sur laquelle on a cliqué
-    
+    const clickedImage = event.target;
+
     // Récupérer l'ID (title) de l'image cliquée
-    const imageTitle = clickedImage.getAttribute("title"); // Prend le titre comme identifiant
-    
+    const imageTitle = clickedImage.getAttribute("title");
+
     // Si les informations sont présentes dans le JSON, les insérer dans le modal
     if (modalData[imageTitle]) {
         modalName.textContent = modalData[imageTitle].name;
@@ -90,16 +105,22 @@ function openModal(event) {
     }
 
     // Mettre l'image cliquée dans le modal
-    modalImage.src = clickedImage.src;  // Prendre la source de l'image cliquée
-    
+    modalImage.src = clickedImage.src;
+
     // Afficher le modal
     modal.style.display = "flex";
+    
+    // Désactiver le scroll de l'arrière-plan
+    document.body.classList.add('modal-open');
 }
 
 // Fonction pour fermer le modal
 function closeModal() {
     const modal = document.getElementById("myModal");
     modal.style.display = "none";  // Masquer le modal
+    
+    // Réactiver le scroll de l'arrière-plan
+    document.body.classList.remove('modal-open');
 }
 
 // Ouvrir le modal lorsque l'on clique sur une image avec un data-id contenant "using-modal"
