2bc7ef2eb209c1cd3ed91a4e7a8ecf23e974a600
mmatteo <matteo.martin@ynov.com>
2025-01-24 13:48:12 +0100

    some descriptions

--- site/js/modal.js
+++ site/js/modal.js
@@ -1,15 +1,43 @@
 const modalData = {
     "2011": {
         "name": "Minecon 2011",
-        "description": "Cette cape a été remise aux participants de la MINECON 2011, à Las Vegas le 7 avril 2011, La cape leurs étaient rémise par apport à leurs nom, c'était la première fois que cette méthode fut utilisé, et aussi la dernière."
+        "date": "7 avril 2011, à Las Vegas au États-Unis",
+        "description": "Cette cape a été remise aux participants de la Minecon 2011. Elle était attribuée en fonction de leur pseudonyme, marquant la première et dernière fois que cette méthode a été utilisée."
     },
     "2012": {
         "name": "Minecon 2012",
-        "description": "Cette cape a été remise aux participants de la MINECON 2012, à Disneyland Paris le 18 juillet 2012, les joueurs ont été remit d'un code par mail qu'ils devaient mettre sur le site minecraft.net pour récupéré leur récompense."
+        "date": "18 juillet 2012, à Disneyland Paris en France",
+        "description": "Cette cape a été remise aux participants de la Minecon 2012. Les joueurs recevaient un code par e-mail qu'ils devaient entrer sur le site minecraft.net pour récupérer leur récompense."
     },
     "2013": {
         "name": "Minecon 2013",
-        "description": "Cette cape a été remise aux participants de la MINECON 2013, à Oralando en Floride le 1er juillet 2013, les joueurs ont été remit d'un code par mail qu'ils devaient mettre sur le site minecraft.net pour récupéré leur récompense."
+        "date": "1er juillet 2013, à Orlando en Floride",
+        "description": "Cette cape a été remise aux participants de la Minecon 2013. Les joueurs recevaient un code par e-mail qu'ils devaient entrer sur le site minecraft.net pour récupérer leur récompense."
+    },
+    "2015": {
+        "name": "Minecon 2015",
+        "date": "4 et 5 juillet 2015, à Londres au Royaume-Uni",
+        "description": "Cette cape a été remise aux participants de la Minecon 2013. Les joueurs recevaient un code par e-mail uniquement après avoir scanné leur billet sur place, qu'ils devaient ensuite entrer sur le site minecraft.net pour récupérer leur récompense."
+    },
+    "2016": {
+        "name": "Minecon 2016",
+        "date": "24 et 25 septembre 2016, à Anaheim au États-Unis",
+        "description": "Cette cape a été remise aux participants de la Minecon 2013. Les joueurs recevaient un code par e-mail uniquement après avoir scanné leur billet sur place, qu'ils devaient ensuite entrer sur le site minecraft.net pour récupérer leur récompense."
+    },
+    "mojangold": {
+        "name": "Cape Mojang (classique)",
+        "date": "20 decembre 2010 au 7 octobre 2015",
+        "description": "Cette cape a été remise aux employés de Mojang Studios entre ces deux dates."
+    },
+    "mojang": {
+        "name": "Cape Mojang",
+        "date": "7 octobre 2015 au 26 juillet 2021",
+        "description": "Cette cape a été attribuée aux employés de Mojang Studios entre ces deux dates. Sa couleur a été modifiée pour mieux correspondre à la nouvelle identité visuelle de Mojang Studios."
+    },
+    "mojangstudios": {
+        "name": "Cape Mojang Studios",
+        "date": "26 juillet 2021 jusqu'à aujourd'hui",
+        "description": "Cette cape a été attribuée aux employés de Mojang Studios entre ces deux dates. Son design a été modifié pour mieux refléter le rebranding du logo réalisé par Johan Aronson."
     },
 };
 
@@ -18,6 +46,7 @@ function openModal(event) {
     const modal = document.getElementById("myModal");
     const modalImage = document.querySelector(".modal-image");
     const modalName = document.querySelector(".modal-name");
+    const modalDate = document.querySelector(".modal-date");
     const modalDesc = document.querySelector(".modal-desc");
     const clickedImage = event.target;  // L'image sur laquelle on a cliqué
     
@@ -27,9 +56,11 @@ function openModal(event) {
     // Si les informations sont présentes dans le JSON, les insérer dans le modal
     if (modalData[imageTitle]) {
         modalName.textContent = modalData[imageTitle].name;
+        modalDate.textContent = modalData[imageTitle].date;
         modalDesc.textContent = modalData[imageTitle].description;
     } else {
         modalName.textContent = "Nom non trouvé";
+        modalDate.textContent = "Date non trouvée";
         modalDesc.textContent = "Description non disponible";
     }
 
