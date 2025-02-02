095e4a5df9e41eb7e4a8142c5c960e88382b74c8
MatteoMYnov <matteo.martin@ynov.com>
2025-02-02 20:40:52 +0100

    better friend list

--- main.go
+++ main.go
@@ -298,7 +298,7 @@ func main() {
 	http.HandleFunc("/menu", menuHandler)
 	http.HandleFunc("/capes", capesHandler)
 
-	if err := http.ListenAndServe(":1614", nil); err != nil {
+	if err := http.ListenAndServe(":1615", nil); err != nil {
 		log.Fatalf("Erreur lors du démarrage du serveur: %v", err)
 	}
 }
