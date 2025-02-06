5ad58fa9f026c4cbaf24ec7e6d066839c649ec57
MatteoMYnov <matteo.martin@ynov.com>
2025-02-06 22:42:14 +0100

    rolllssssss

--- site/js/scroll.js
+++ site/js/scroll.js
@@ -3,7 +3,7 @@ window.addEventListener("scroll", function () {
     const maxScroll = document.body.scrollHeight - window.innerHeight;
 
     let opacity = Math.min(scrollPosition / (maxScroll * 0.5), 1);
-    let blurValue = Math.min(scrollPosition / (maxScroll * 0.3) * 5, 5); // Flou max de 10px
+    let blurValue = Math.min(scrollPosition / (maxScroll * 0.5) * 5, 5); // Flou max de 10px
 
     const bgdark = document.getElementById("bgdark");
     if (bgdark) {
