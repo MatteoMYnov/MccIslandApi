0b3827ba6b86e935f9e67174da8eb78b52e2f3bf
mmatteo <matteo.martin@ynov.com>
2025-02-06 08:28:47 +0100

    cursor fix

--- main.go
+++ main.go
@@ -477,7 +477,7 @@ func main() {
 	// Redirection de /classement vers /classement/1
 	http.HandleFunc("/classement/", classementHandler)
 
-	if err := http.ListenAndServe(":1603", nil); err != nil {
+	if err := http.ListenAndServe(":1604", nil); err != nil {
 		log.Fatalf("Erreur lors du démarrage du serveur: %v", err)
 	}
 }
