c1e0618b89607bbfda6071f774526bd82d118809
MatteoMYnov <matteo.martin@ynov.com>
2025-02-17 17:59:05 +0100

    equip cosmetic render

--- main.go
+++ main.go
@@ -541,7 +541,7 @@ func main() {
 	// Redirection de /classement vers /classement/1
 	http.HandleFunc("/classement/", classementHandler)
 
-	if err := http.ListenAndServe(":1605", nil); err != nil {
+	if err := http.ListenAndServe(":1618", nil); err != nil {
 		log.Fatalf("Erreur lors du démarrage du serveur: %v", err)
 	}
 }
