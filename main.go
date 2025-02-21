049190003b0a88e51c8ec7dbf6daf3d24cc5f6a8
MatteoMYnov <matteo.martin@ynov.com>
2025-02-21 09:57:12 +0100

    debug for equipped cos and accessories files

--- main.go
+++ main.go
@@ -587,7 +587,7 @@ func main() {
 	// Redirection de /classement vers /classement/1
 	http.HandleFunc("/classement/", classementHandler)
 
-	if err := http.ListenAndServe(":1616", nil); err != nil {
+	if err := http.ListenAndServe(":1601", nil); err != nil {
 		log.Fatalf("Erreur lors du démarrage du serveur: %v", err)
 	}
 }
